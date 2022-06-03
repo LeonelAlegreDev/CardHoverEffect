e74a61b0aeccce6bb5c6f85cf84de9fd73c9dcc9
Leonel Alegre <leonelalegre.dev@gmail.com>
2022-06-03 12:28:59 -0300

    Card Hover Effect Whit JS

---
+++ js/main.js
@@ -0,0 +1,49 @@
+const cards = document.getElementsByClassName('card');
+
+for(let i = 0; i < cards.length; i++){
+    hoverEffect(cards[i])
+}
+
+function hoverEffect(selected){
+    selected.addEventListener("mouseenter", function(event){
+        let selected = event.target;
+        let cardArticle = selected.firstElementChild.firstElementChild.firstElementChild;
+        let cardImg = selected.lastElementChild;
+
+        animationEnter(selected, cardArticle, cardImg);
+    });
+    selected.addEventListener("mouseleave", function(event){
+        let selected = event.target;
+        let cardArticle = selected.firstElementChild.firstElementChild.firstElementChild;
+        let cardImg = event.target.lastElementChild;
+
+        animationLeave(selected, cardArticle, cardImg);
+    });        
+}
+function animationEnter(selected, cardArticle, cardImg){
+    //image animation
+    cardImg.style.transform = "scale(1.3) translateX(80px)";
+    //card animation
+    selected.style.backgroundColor = "rgb(255, 195, 106)";
+    selected.style.width = "500px";
+    selected.style.height = "250px";
+    selected.style.borderRadius = "10px";
+    //article animation
+    cardArticle.style.display= "flex";
+    setTimeout(function()
+    {
+        cardArticle.style.transform= "translateX(0)";
+    }, 200);
+}
+function animationLeave(selected, cardArticle, cardImg){
+    //image animation
+    cardImg.style.transform = "scale(1) translateX(0)";
+    //card animation
+    selected.style.backgroundColor = "rgb(0, 28, 10)";
+    selected.style.width = "250px";
+    selected.style.height = "250px";
+    selected.style.borderRadius = "100%";
+    //article animtion
+    cardArticle.style.transform= "translateX(100%)";
+    cardArticle.style.display= "none";
+}
